f3f177a74e55dd38df03a0e1689308c63587e416
Nick Wohnhas <npw7887@gmail.com>
2019-09-22 08:42:10 -0400

    Remove unused state related to GameOver modal

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,13 +1,10 @@
-import React, { useState, useEffect} from 'react'
+import React, { useEffect } from 'react'
 import '../styles/GameOver.css'
 
 const GameOver = ({startGame, score}) => {
 
-const [showModal, setShowModal] = useState(true)
-
 const handleKeyDown = (e) => {
   if (e.keyCode == 13) {
-    setShowModal(false)
     startGame()
   }
 }
@@ -21,9 +18,9 @@ const handleKeyDown = (e) => {
 
   return (
     <div className="game-over">
-      <h1 class="game-over-header">Game Over</h1>
-      <h2>Your score: <span class="score">{score}</span></h2>
-      <h2>Press <span class="enter">Enter</span> to play again</h2>
+      <h1 className="game-over-header">Game Over</h1>
+      <h2>Your score: <span className="score">{score}</span></h2>
+      <h2>Press <span className="enter">Enter</span> to play again</h2>
     </div>
   )
 }
