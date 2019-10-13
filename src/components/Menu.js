35171ada1360a858747b5731a8f61b8edec6040b
Nick Wohnhas <npw7887@gmail.com>
2019-10-13 15:47:47 -0400

    Format control conditional rendering in menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -22,8 +22,13 @@ const Menu = () => {
     <div className="container">
       <h1 className="item">Greetings Cadet</h1>
       <h2 className="item">Press <span className="option">Enter</span> for new game</h2>
-      {showControls ? <div className="controls"><p>Move spaceship up: "d"</p>
-        <p>Move spaceship down: "c"</p></div> : <h2 className="item"> Press <span className="option">c</span> for controls</h2 > }
+      {showControls ? (
+      <div className="controls">
+      <p>Move spaceship up: "d"</p>
+      <p>Move spaceship down: "c"</p></div>
+      ) : (
+      <h2 className="item"> Press <span className="option">c</span> for controls</h2 >
+      )}
     </div>
   )
 }
